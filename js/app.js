0d4d365904e4d680cc4cc40c1d1b5bf8e93665bf
CMRWebDev <crosenberg690@gmail.com>
2015-10-13 22:31:34 -0400

    jQuery intial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	// validate enter items text box and enter quantityt text box
+// use enter to add list items
+	
+
+$("#btnSubmit").click(function(){
+        alert("button");
+			
+
+	// add enter items to items list
+	// add enter quantity to quantity list
+
+
+	});
+});
+
