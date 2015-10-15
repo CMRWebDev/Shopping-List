bbf99aec8c7d5ffb09686f99939d9247906ea182
CMRWebDev <crosenberg690@gmail.com>
2015-10-14 21:27:34 -0400

    jQuery

--- js/app.js
+++ js/app.js
@@ -15,10 +15,19 @@ $("#add-todo").click(function(){
 		if(!$.trim($('#add-item').val())) {
 			alert('Please enter text to add to the list');
 		} else {
-			$('<ul class="items"></ul>').appendTo('#list').html('<div class="box"></div><span>' + txtval + '</span><img class="delete" src="images/delete.png"/>');
+			$('<ul class="items"></ul>').appendTo('.items').html(itemsval);
 
-		document.getElementById('item').value = '';
+		document.getElementById('add-item').value = '';
+		};
+
+		if(!$.trim($('#add-quantity').val())) {
+			alert('Please enter text to add to the list');
+		} else {
+			$('<ul class="quantity"></ul>').appendTo('.quantity').html(quantityval);
+
+		document.getElementById('add-quantity').value = '';
 		};	
+			
 
 	// add enter items to items list
 	// add enter quantity to quantity list
