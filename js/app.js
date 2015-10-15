1d6ec022ee9e8cca832596e1e22026c95f32fd44
CMRWebDev <crosenberg690@gmail.com>
2015-10-15 19:44:38 -0400

    Getting there

--- js/app.js
+++ js/app.js
@@ -5,35 +5,50 @@ $(document).ready(function() {
 	
 // add item and quantity to todo list
 $("#add-todo").click(function(){
-		// Item variables
+		
+		// Global Variables
         var textbox_items = document.getElementById('add-item')
         var itemsval = textbox_items.value;
+        var checkBox = '<span class="check">&#10004</span>'
+        var xMark = '<span class="delete">&#10008</span>'
         // Quantity variables
-        var textbox_quantity = document.getElementById('add-quantity')
-        var quantityval = textbox_quantity.value;
-		
+        
+		// Adds items to list after user enters them in text box
 		if(!$.trim($('#add-item').val())) {
 			alert('Please enter text to add to the list');
 		} else {
-			$('<li class="item"></li>').appendTo('.items-list').html(itemsval);
+			$('<li class="item"></li>').appendTo('.items-list').html(checkBox + itemsval + xMark);
 
 		document.getElementById('add-item').value = '';
 		};
 
-		
-		if(!$.trim($('#add-quantity').val())) {
-			alert('Please enter text to add to the list');
-		} else {
-			$('<li class="quantity"></li>').appendTo('.quantity-list').html(quantityval);
 
-		document.getElementById('add-quantity').value = '';
-		};	
+		
+	
 			
 
-	// add enter items to items list
-	// add enter quantity to quantity list
+	
 
 
 	});
 });
+
+// When user clicks delete item is removed from list
+$(document).on("click", ".delete", function(){
+	$(this).closest('li').fadeOut(300);
+});
+
+// Allows user to check off items
+
+
+
+// When delete is clixked
+// remove item from list
+
+
+// When check is clicked
+// Put line through
+// make see through
+
+
 
